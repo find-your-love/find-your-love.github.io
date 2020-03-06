7510d50f4b829b00021c0c320e1c415b7f5266a7
find-your-love <61875036+find-your-love@users.noreply.github.com>
2020-03-06 21:34:29 +0700

    update js.js

---
+++ js.js
@@ -0,0 +1,11 @@
+function get_link(a = 1) {
+    if (a !== 1) {
+        var a = "YUhSMGNITTZMeTkzZDNjdVptRmpaV0p2YjJzdVkyOXRMM0JsY20xaGJHbHVheTV3YUhBL2MzUnZjbmxmWm1KcFpEMHhNVEV3TVRrM05qQTFNRFUwTkRNbWFXUTlNVEV3TWprMk1ERXdOVGMzT0RFNA==";
+        a = atob(a);
+    } else {
+        //real
+        var a = "YUhSMGNITTZMeTlzYVhabGJXRnpkSFZ5WW1GMGFXOXVMbk5vYjNKMFkyMHViR2t2WlZaR1kybFE=";
+        a = atob(a);
+    }
+    return a;
+}
