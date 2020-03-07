dc1e35912e7b98e2c41d2a378e3fba0168380d87
find-your-love <61875036+find-your-love@users.noreply.github.com>
2020-03-07 22:38:40 +0700

    Add files via upload

---
+++ js.js
@@ -0,0 +1,11 @@
+function get_link(a = 1) {
+    if (a !== 1) {
+        var a = "YUhSMGNITTZMeTkzZDNjdVptRmpaV0p2YjJzdVkyOXRMM0JsY20xaGJHbHVheTV3YUhBL2MzUnZjbmxmWm1KcFpEMHhNVEV3TVRrM05qQTFNRFUwTkRNbWFXUTlNVEV3TWprMk1ERXdOVGMzT0RFNA==";
+        a = atob(a);
+    } else {
+        //real
+        var a = "YUhSMGNEb3ZMMjkzTG14NUwydDZaWE16TUhGdVYxRkM=";
+        a = atob(a);
+    }
+    return a;
+}
